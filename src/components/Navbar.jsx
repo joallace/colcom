7e8deebad101723ab9c7f049d6db40b268cd8bea
joallace <wallace.lucena@hotmail.com>
2023-09-12 16:09:26 -0300

    Updates in the mock navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,28 @@ import '../assets/sass/navbar.scss'
 export default function Navbar() {
   return (
     <nav className="nav">
-      <a href="/" className="nav-icon">
-        Col.com
-      </a>
-      <ul>
-        <li>
-          <a href="/promoted">promovido</a>
-        </li>
-        <li>
-          <a href="/all">todos</a>
-        </li>
-        <li>
-          <a href="/meta">meta</a>
-        </li>
-      </ul>
+      <div style={{display:'flex'}}>
+        <a href="/" className="nav-icon">
+          col<text style={{color: 'white'}}>.</text><text style={{color: '#BC5217'}}>com</text>
+        </a>
+        <ul>
+          <li>
+            <a href="/promoted">promovido</a>
+          </li>
+          <li>
+            <a href="/all">todos</a>
+          </li>
+          <li>
+            <a href="/meta">meta</a>
+          </li>
+        </ul>
+      </div>
+      <div style={{gap: "0.75rem"}}>
+        <input className="nav-searchbar" placeholder="pesquisar..."/>
+        <text style={{fontWeight: "bolder", fontSize: "1.5rem"}}>+</text>
+        $42
+        <div className="nav-user-icon"/>
+      </div>
     </nav>
   )
 }
