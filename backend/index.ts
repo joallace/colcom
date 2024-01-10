cda81b21fa8192cb4a080b689eb143e41c955d92
joallace <wallace.lucena@hotmail.com>
2024-01-10 19:22:28 -0300

    Changing return type of get topics from object to array, and adding check for duplicity in topic title

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import fs from "fs"
 import { execSync } from "child_process"
-import { v4 as uuid } from "uuid"
+import { v5 as uuid } from "uuid"
 
 const app = express()
 const port = 3000
 const dbPath = process.env.DB_PATH || "/db"
+const namespace = process.env.UUID_NAMESPACE || ""
 
 const gitLogParameters = `--pretty=format:'{^^^^commit^^^^:^^^^%h^^^^,^^^^subject^^^^:^^^^%s^^^^,^^^^date^^^^:^^^^%aD^^^^,^^^^author^^^^:^^^^%aN^^^^},' | sed 's/"/\\\\"/g' | sed 's/\\^^^^/"/g' | sed "$ s/,$//" | sed -e ':a' -e 'N' -e '$!ba' -e 's/\\n/ /g'  | awk 'BEGIN { print("[") } { print($0) } END { print("]") }'`
 
@@ -18,14 +19,13 @@ app.get("/", (req, res) => {
 app.get("/topics", (req, res) => {
   const page = Number(req.query.page) || 0
   const pageSize = Number(req.query.pageSize) || 10
-  let result: any = {}
+  let result: any = []
 
-  const topicsIds = fs.readdirSync(dbPath).slice(page, (page+1)*pageSize)
-  console.log(topicsIds)
+  const topicsIds = fs.readdirSync(dbPath).slice(page, (page + 1) * pageSize)
   topicsIds.forEach(id => {
-    result[id] = JSON.parse(fs.readFileSync(`${dbPath}/${id}/meta.json`,  { encoding: "utf-8"}))
+    result.push(JSON.parse(fs.readFileSync(`${dbPath}/${id}/meta.json`, { encoding: "utf-8" })))
   })
-  
+
   res.status(200).json(result)
 })
 
@@ -33,12 +33,17 @@ app.use("/topics", express.static(dbPath))
 
 app.post("/topics", (req, res) => {
   const { title } = req.body
-  const path = `${dbPath}/${title}`
+  const id = uuid(title, namespace)
+  const path = `${dbPath}/${id}`
 
-  const result = execSync(`mkdir "${path}" && git -C "${path}" init && echo $?`, { encoding: "utf-8"})
+  if (fs.existsSync(path))
+    res.status(409).send("Topic already exists")
+
+  const result = execSync(`mkdir "${path}" && git -C "${path}" init && echo $?`, { encoding: "utf-8" })
   const status = result.slice(-2, -1) === "0" ? 201 : 500
 
   const metaData = {
+    id,
     title,
     up: 0,
     down: 0,
