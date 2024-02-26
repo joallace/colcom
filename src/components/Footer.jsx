8510ceec425d20695bfe98697125767272b2a136
joallace <wallace.lucena@hotmail.com>
2024-02-26 11:43:41 -0300

    First footer implementation

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { PiGithubLogoFill } from "react-icons/pi"
+
+export default () => (
+    <footer>
+        <a href="https://github.com/joallace/colcom"><PiGithubLogoFill /> GitHub</a>
+    </footer>
+)
