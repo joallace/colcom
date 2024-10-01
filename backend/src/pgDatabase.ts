cfe2725e8c02181532f342e9d47ec61645ddb457
joallace <wallace.lucena@hotmail.com>
2024-09-30 21:42:42 -0300

    chore: Simplifying passage of Postgres port through docker-compose.yml

--- backend/src/pgDatabase.ts
+++ backend/src/pgDatabase.ts
@@ -14,7 +14,7 @@ const config = {
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   host: process.env.POSTGRES_HOST || "localhost",
-  port: Number(process.env.POSTGRES_PORT) || 5432,
+  port: Number(process.env.PGPORT) || 5432,
   database: process.env.POSTGRES_DB || "colcom",
   max: Number(process.env.DB_POOL) || 200,
   idleTimeoutMillis: 0,
