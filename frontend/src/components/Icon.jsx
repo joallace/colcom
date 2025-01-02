d00ebb9b6019b41b76103bd40b7cdf17ef42022c
joallace <wallace.lucena@hotmail.com>
2025-01-01 22:35:07 -0300

    style(front-end): Changing theme to cyan with black background

--- frontend/src/components/Icon.jsx
+++ frontend/src/components/Icon.jsx
@@ -1,8 +1,8 @@
 export default function Icon() {
     return (
-        <span style={{ display: 'flex', flexWrap: 'wrap', color: '#91825a', lineHeight: '1.2rem' }}>col
+        <span style={{ display: 'flex', flexWrap: 'wrap', color: 'rgb(29, 155, 240)', lineHeight: '1.2rem' }}>col
             <span style={{ color: 'white' }}>.</span>
-            <span style={{ color: '#BC5217' }}>com</span>
+            <span style={{ color: 'rgb(113, 118, 123)' }}>com</span>
         </span>
     )
 }
